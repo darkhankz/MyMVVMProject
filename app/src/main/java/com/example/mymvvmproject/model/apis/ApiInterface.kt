5b1d3541e4dbcf53bc334641225dfb58a3f7fa5b
darkhankz <upstudynow@gmail.com>
2022-09-10 19:01:33 +0600

    response-check

--- app/src/main/java/com/example/mymvvmproject/model/apis/ApiInterface.kt
+++ app/src/main/java/com/example/mymvvmproject/model/apis/ApiInterface.kt
@@ -2,6 +2,7 @@ package com.example.mymvvmproject.model.apis
 
 import com.example.mymvvmproject.data.GetMoviesResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -12,7 +13,7 @@ interface ApiInterface {
     fun getPopularMovies(
         @Query("api_key") apiKey: String = "6e76ecffda0a59dc4f19a343c6e7648a",
         @Query("page") page: Int
-    ): Call<GetMoviesResponse>
+    ): Response<GetMoviesResponse>
 
     companion object{
         private const val BASE_URL = "https://api.themoviedb.org/"
